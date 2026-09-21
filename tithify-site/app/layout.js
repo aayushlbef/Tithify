133d7988de1897f8dbfede94f315e654eb53321b
aayushlbef <aayush.gupta@study.lbef.edu.np>
2026-09-21 18:45:02 +0545

    Updatating website

--- tithify-site/app/layout.js
+++ tithify-site/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script";
 import { Sora, Inter, Noto_Sans_Devanagari } from "next/font/google";
 import { FAQS } from "@/lib/faqs";
 
@@ -269,7 +270,21 @@ export default function RootLayout({ children }) {
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-2MZEHM7RY6"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-2MZEHM7RY6');
+          `}
+        </Script>
+      </body>
     </html>
   );
 }
